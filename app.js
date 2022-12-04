bbe82587f84b20d4193ef29826fbe63d5406c613
rinshadcp <rinshadcp7337@gmail.com>
2022-12-04 15:59:51 +0530

    Exprss server added

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+const path= require("path");
+
+
+
+
+const app = express();
+
+
+
+
+
+module.exports=app;
---
+++ server.js
@@ -0,0 +1,25 @@
+
+const mongoose = require ("mongoose");
+const dotenv = require ("dotenv");
+const app =require("./app");
+
+
+
+dotenv.config({ path: './config.env' });
+
+//Connect to  database ...
+const DB=process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD);
+    mongoose.connect(DB, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+    }, err => {
+    if(err) throw err;
+    console.log('  Connected to MongoDB!!!')
+    });
+// start server
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
